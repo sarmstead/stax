c86efcb685176f35b266eafd63b440d3b6e7a743
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2024-07-03 21:36:45 -0400

    Integrate and configure Tailwind

---
+++ config/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
